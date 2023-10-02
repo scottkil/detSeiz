ebbc47d338bb3fdf93513ff76ad40273d170971d
unknown <scottkilianski@gmail.com>
2023-10-02 10:26:52 -0400

    Updating environment to include pyABF package

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 23:08:06 2023
+
+@author: Scott
+"""
+from setuptools import find_packages, setup
+
+setup(
+	name='src',
+	packages=find_packages(),
+    )
